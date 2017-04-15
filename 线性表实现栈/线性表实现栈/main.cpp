cf0b7261ef458d7fec0316946e3d57e36d44abc8
ya-ning <wangyaning6166@gmail.com>
2017-04-15 11:29:51 +0800

    牺牲链头，换取pop操作方便，挺值的。

--- 线性表实现栈/线性表实现栈/main.cpp
+++ 线性表实现栈/线性表实现栈/main.cpp
@@ -8,16 +8,20 @@ typedef struct Node {
 typedef Node* Nodep;
 
 int Pop(Nodep head);
-Nodep Push(Nodep head, int data);
+void Push(Nodep head, int data);
 Nodep Create(int data);
 
 int main(void) {
-	Nodep head = NULL;
-	head = Push(head, 3);
+	Nodep head = Create(-1);
+	Push(head, 3);
 	for (int i = 0; i < 10; i++) {
 		Push(head, i);
 	}
-	for (int i = 0; i < 12; i++) {
+	for (int i = 0; i < 6; i++) {
+		printf("%d ", Pop(head));
+	}
+	printf("\n");
+	for (int i = 0; i < 5; i++) {
 		printf("%d ", Pop(head));
 	}
 	return 0;
@@ -33,36 +37,26 @@ Nodep Create(int data) {
 int Pop(const Nodep head) {
 	static Nodep temp = NULL, last = NULL;
 	int data;
-	temp = head;
-	if (temp == NULL) {
-		printf("链表为空，无法弹出元素！\n");
-		return -1;
-	}
-	last = head->next;
-	if (last == NULL) {
-		printf("链表为空，无法弹出元素！\n");
-		return -1;
-	}
+	temp = last = head;
 	while (last->next != NULL) {
 		temp = last;
 		last = last->next;
 	}
+	if (temp == head) {
+		printf("\n空表无法弹出元素！");
+		return -1;
+	}
 	data = last->data;
-	temp->next = NULL;
 	free(last);
+	temp->next = NULL;
 	return data;
 }
 
-Nodep Push(const Nodep head, const int data) {
+void Push(const Nodep head, const int data) {
 	static Nodep temp = NULL;
 	temp = head;
-	if (temp == NULL) {
-		temp = Create(data);
-		return temp;
-	}
 	while (temp->next != NULL) {
 		temp = temp->next;
 	}
 	temp->next = Create(data);
-	return temp;
 }
