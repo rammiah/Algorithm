04d24912d7aad78bde0270c15aaf085d1849b97a
hustr <wangyaning6166@gmail.com>
2017-08-31 11:54:31 +0800

    fix indent

--- Permutations.cpp
+++ Permutations.cpp
@@ -1,39 +1,31 @@
-nclude <iostream>
+#include <iostream>
 #include <vector>
 
 template<typename T>
 void swap(T &a, T &b) {
-        T t = a;
-        a = b;
-        b = t;
-
+    T t = a;
+    a = b;
+    b = t;
 }
 
 void search(std::vector<int> &v, const int &index) {
     if (index == v.size() - 1) {
         for (const int &t : v) {
-                        std::cout << t << " ";
-                    
+            std::cout << t << " ";
+
         }
-                std::cout << std::endl;
-            
+        std::cout << std::endl;
     }
 
     for (int i = index; i < v.size(); ++i) {
-                swap<int>(v[i], v[index]);
-                search(v, index + 1);
-                swap<int>(v[i], v[index]);
-            
+        swap<int>(v[i], v[index]);
+        search(v, index + 1);
+        swap<int>(v[i], v[index]);
     }
-
 }
 
 int main() {
-        std::vector<int> v = {1, 2, 3, 4, 5, 6, 7};
-
-        search(v, 0);
-
-        return 0;
-
+    std::vector<int> v = {1, 2, 3, 4, 5, 6, 7};
+    search(v, 0);
+    return 0;
 }
-
