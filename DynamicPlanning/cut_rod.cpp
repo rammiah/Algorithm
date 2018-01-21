148d92ae14be798e746f06122b8ea1d77350f653
hustr <wangyaning6166@gmail.com>
2018-01-21 11:52:02 +0800

    comment

--- DynamicPlanning/cut_rod.cpp
+++ DynamicPlanning/cut_rod.cpp
@@ -7,12 +7,14 @@ constexpr int size = 11;
 
 int values[size] = { 0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
 
+// 根据给定的大小计算可以得到的最大价值，动态规划
 int max_value(int count) {
     auto help_arr = new int[count + 1];
     help_arr[0] = 0;
     int max = 0;
     for (int i = 1; i <= count; ++i) {
         max = 0;
+        // 根据已知计算现在的
         for (int j = 1; j <= i; ++j) {
             max = std::max(max, help_arr[i - j] + values[j]);
         }
